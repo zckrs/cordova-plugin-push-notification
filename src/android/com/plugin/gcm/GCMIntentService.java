80267eb34e263db9862c9b4402c22b32bd531b4b
Mehdy Dara <mdara@eleven-labs.com>
2015-06-22 14:20:49 +0200

    Add tricky support for Android5
    You should place a png file "notification_icon.png" in platforms/android/res/raw/notification_icon.png

--- src/android/com/plugin/gcm/GCMIntentService.java
+++ src/android/com/plugin/gcm/GCMIntentService.java
@@ -9,9 +9,11 @@ import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.support.v4.app.NotificationCompat;
 import android.util.Log;
+import android.os.Build;
 
 import com.google.android.gcm.GCMBaseIntentService;
 
@@ -94,7 +96,7 @@ public class GCMIntentService extends GCMBaseIntentService {
 
     NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(context)
       .setDefaults(defaults)
-      .setSmallIcon(context.getApplicationInfo().icon)
+      .setSmallIcon(getNotificationIcon(context))
       .setWhen(System.currentTimeMillis())
       .setContentTitle(extras.getString("title"))
       .setTicker(extras.getString("title"))
@@ -131,6 +133,18 @@ public class GCMIntentService extends GCMBaseIntentService {
       .getApplicationLabel(context.getApplicationInfo());
 
     return (String) appName;
+  }
+
+  private int getNotificationIcon(Context context) {
+    boolean isLollipop = Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP;
+
+    if (isLollipop) {
+      Resources r = getResources();
+
+      return r.getIdentifier("notification_icon", "raw", context.getPackageName());
+    }
+
+    return context.getApplicationInfo().icon;
   }
 
   @Override
