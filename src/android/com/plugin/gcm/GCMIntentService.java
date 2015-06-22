4f1b34273af5056f5b838cf8f7d313650712bd13
Mehdy Dara <mdara@eleven-labs.com>
2015-06-22 12:50:15 +0200

    Beautify JAVA file

--- src/android/com/plugin/gcm/GCMIntentService.java
+++ src/android/com/plugin/gcm/GCMIntentService.java
@@ -18,136 +18,124 @@ import com.google.android.gcm.GCMBaseIntentService;
 @SuppressLint("NewApi")
 public class GCMIntentService extends GCMBaseIntentService {
 
-	private static final String TAG = "GCMIntentService";
-	
-	public GCMIntentService() {
-		super("GCMIntentService");
-	}
+  private static final String TAG = "GCMIntentService";
 
-	@Override
-	public void onRegistered(Context context, String regId) {
+  public GCMIntentService() {
+    super("GCMIntentService");
+  }
 
-		Log.v(TAG, "onRegistered: "+ regId);
+  @Override
+  public void onRegistered(Context context, String regId) {
 
-		JSONObject json;
+    Log.v(TAG, "onRegistered: " + regId);
 
-		try
-		{
-			json = new JSONObject().put("event", "registered");
-			json.put("regid", regId);
+    JSONObject json;
 
-			Log.v(TAG, "onRegistered: " + json.toString());
+    try {
+      json = new JSONObject().put("event", "registered");
+      json.put("regid", regId);
 
-			// Send this JSON data to the JavaScript application above EVENT should be set to the msg type
-			// In this case this is the registration ID
-			PushPlugin.sendJavascript( json );
+      Log.v(TAG, "onRegistered: " + json.toString());
 
-		}
-		catch( JSONException e)
-		{
-			// No message to the user is sent, JSON failed
-			Log.e(TAG, "onRegistered: JSON exception");
-		}
-	}
+      // Send this JSON data to the JavaScript application above EVENT should be set to the msg type
+      // In this case this is the registration ID
+      PushPlugin.sendJavascript(json);
 
-	@Override
-	public void onUnregistered(Context context, String regId) {
-		Log.d(TAG, "onUnregistered - regId: " + regId);
-	}
+    } catch (JSONException e) {
+      // No message to the user is sent, JSON failed
+      Log.e(TAG, "onRegistered: JSON exception");
+    }
+  }
 
-	@Override
-	protected void onMessage(Context context, Intent intent) {
-		Log.d(TAG, "onMessage - context: " + context);
+  @Override
+  public void onUnregistered(Context context, String regId) {
+    Log.d(TAG, "onUnregistered - regId: " + regId);
+  }
 
-		// Extract the payload from the message
-		Bundle extras = intent.getExtras();
-		if (extras != null)
-		{
-			// if we are in the foreground, just surface the payload, else post it to the statusbar
-            if (PushPlugin.isInForeground()) {
-				extras.putBoolean("foreground", true);
-                PushPlugin.sendExtras(extras);
-			}
-			else {
-				extras.putBoolean("foreground", false);
+  @Override
+  protected void onMessage(Context context, Intent intent) {
+    Log.d(TAG, "onMessage - context: " + context);
 
-                // Send a notification if there is a message
-                if (extras.getString("message") != null && extras.getString("message").length() != 0) {
-                    createNotification(context, extras);
-                }
-            }
+    // Extract the payload from the message
+    Bundle extras = intent.getExtras();
+    if (extras != null) {
+      // if we are in the foreground, just surface the payload, else post it to the statusbar
+      if (PushPlugin.isInForeground()) {
+        extras.putBoolean("foreground", true);
+        PushPlugin.sendExtras(extras);
+      } else {
+        extras.putBoolean("foreground", false);
+
+        // Send a notification if there is a message
+        if (extras.getString("message") != null && extras.getString("message").length() != 0) {
+          createNotification(context, extras);
         }
-	}
+      }
+    }
+  }
 
-	public void createNotification(Context context, Bundle extras)
-	{
-		NotificationManager mNotificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-		String appName = getAppName(this);
+  public void createNotification(Context context, Bundle extras) {
+    NotificationManager mNotificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+    String appName = getAppName(this);
 
-		Intent notificationIntent = new Intent(this, PushHandlerActivity.class);
-		notificationIntent.addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP | Intent.FLAG_ACTIVITY_CLEAR_TOP);
-		notificationIntent.putExtra("pushBundle", extras);
+    Intent notificationIntent = new Intent(this, PushHandlerActivity.class);
+    notificationIntent.addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP | Intent.FLAG_ACTIVITY_CLEAR_TOP);
+    notificationIntent.putExtra("pushBundle", extras);
 
-		PendingIntent contentIntent = PendingIntent.getActivity(this, 0, notificationIntent, PendingIntent.FLAG_UPDATE_CURRENT);
-		
-		int defaults = Notification.DEFAULT_ALL;
+    PendingIntent contentIntent = PendingIntent.getActivity(this, 0, notificationIntent, PendingIntent.FLAG_UPDATE_CURRENT);
 
-		if (extras.getString("defaults") != null) {
-			try {
-				defaults = Integer.parseInt(extras.getString("defaults"));
-			} catch (NumberFormatException e) {}
-		}
-		
-		NotificationCompat.Builder mBuilder =
-			new NotificationCompat.Builder(context)
-				.setDefaults(defaults)
-				.setSmallIcon(context.getApplicationInfo().icon)
-				.setWhen(System.currentTimeMillis())
-				.setContentTitle(extras.getString("title"))
-				.setTicker(extras.getString("title"))
-				.setContentIntent(contentIntent)
-				.setAutoCancel(true);
+    int defaults = Notification.DEFAULT_ALL;
 
-		String message = extras.getString("message");
-		if (message != null) {
-			mBuilder.setContentText(message);
-		} else {
-			mBuilder.setContentText("<missing message content>");
-		}
+    if (extras.getString("defaults") != null) {
+      try {
+        defaults = Integer.parseInt(extras.getString("defaults"));
+      } catch (NumberFormatException e) {}
+    }
 
-		String msgcnt = extras.getString("msgcnt");
-		if (msgcnt != null) {
-			mBuilder.setNumber(Integer.parseInt(msgcnt));
-		}
-		
-		int notId = 0;
-		
-		try {
-			notId = Integer.parseInt(extras.getString("notId"));
-		}
-		catch(NumberFormatException e) {
-			Log.e(TAG, "Number format exception - Error parsing Notification ID: " + e.getMessage());
-		}
-		catch(Exception e) {
-			Log.e(TAG, "Number format exception - Error parsing Notification ID" + e.getMessage());
-		}
-		
-		mNotificationManager.notify((String) appName, notId, mBuilder.build());
-	}
-	
-	private static String getAppName(Context context)
-	{
-		CharSequence appName = 
-				context
-					.getPackageManager()
-					.getApplicationLabel(context.getApplicationInfo());
-		
-		return (String)appName;
-	}
-	
-	@Override
-	public void onError(Context context, String errorId) {
-		Log.e(TAG, "onError - errorId: " + errorId);
-	}
+    NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(context)
+      .setDefaults(defaults)
+      .setSmallIcon(context.getApplicationInfo().icon)
+      .setWhen(System.currentTimeMillis())
+      .setContentTitle(extras.getString("title"))
+      .setTicker(extras.getString("title"))
+      .setContentIntent(contentIntent)
+      .setAutoCancel(true);
+
+    String message = extras.getString("message");
+    if (message != null) {
+      mBuilder.setContentText(message);
+    } else {
+      mBuilder.setContentText("<missing message content>");
+    }
+
+    String msgcnt = extras.getString("msgcnt");
+    if (msgcnt != null) {
+      mBuilder.setNumber(Integer.parseInt(msgcnt));
+    }
+
+    int notId = 0;
+
+    try {
+      notId = Integer.parseInt(extras.getString("notId"));
+    } catch (NumberFormatException e) {
+      Log.e(TAG, "Number format exception - Error parsing Notification ID: " + e.getMessage());
+    } catch (Exception e) {
+      Log.e(TAG, "Number format exception - Error parsing Notification ID" + e.getMessage());
+    }
+
+    mNotificationManager.notify((String) appName, notId, mBuilder.build());
+  }
+
+  private static String getAppName(Context context) {
+    CharSequence appName = context.getPackageManager()
+      .getApplicationLabel(context.getApplicationInfo());
+
+    return (String) appName;
+  }
+
+  @Override
+  public void onError(Context context, String errorId) {
+    Log.e(TAG, "onError - errorId: " + errorId);
+  }
 
 }
--- src/android/com/plugin/gcm/PushHandlerActivity.java
+++ src/android/com/plugin/gcm/PushHandlerActivity.java
@@ -8,59 +8,54 @@ import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.util.Log;
 
-public class PushHandlerActivity extends Activity
-{
-	private static String TAG = "PushHandlerActivity"; 
+public class PushHandlerActivity extends Activity {
+  private static String TAG = "PushHandlerActivity";
 
-	/*
-	 * this activity will be started if the user touches a notification that we own. 
-	 * We send it's data off to the push plugin for processing.
-	 * If needed, we boot up the main activity to kickstart the application. 
-	 * @see android.app.Activity#onCreate(android.os.Bundle)
-	 */
-	@Override
-	public void onCreate(Bundle savedInstanceState)
-	{
-		super.onCreate(savedInstanceState);
-		Log.v(TAG, "onCreate");
+  /*
+   * this activity will be started if the user touches a notification that we own.
+   * We send it's data off to the push plugin for processing.
+   * If needed, we boot up the main activity to kickstart the application.
+   * @see android.app.Activity#onCreate(android.os.Bundle)
+   */@Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    Log.v(TAG, "onCreate");
 
-		boolean isPushPluginActive = PushPlugin.isActive();
-		processPushBundle(isPushPluginActive);
+    boolean isPushPluginActive = PushPlugin.isActive();
+    processPushBundle(isPushPluginActive);
 
-		finish();
+    finish();
 
-		if (!isPushPluginActive) {
-			forceMainActivityReload();
-		}
-	}
+    if (!isPushPluginActive) {
+      forceMainActivityReload();
+    }
+  }
 
-	/**
-	 * Takes the pushBundle extras from the intent, 
-	 * and sends it through to the PushPlugin for processing.
-	 */
-	private void processPushBundle(boolean isPushPluginActive)
-	{
-		Bundle extras = getIntent().getExtras();
+  /**
+   * Takes the pushBundle extras from the intent,
+   * and sends it through to the PushPlugin for processing.
+   */
+  private void processPushBundle(boolean isPushPluginActive) {
+    Bundle extras = getIntent().getExtras();
 
-		if (extras != null)	{
-			Bundle originalExtras = extras.getBundle("pushBundle");
-            
-            originalExtras.putBoolean("foreground", false);
-            originalExtras.putBoolean("coldstart", !isPushPluginActive);
+    if (extras != null) {
+      Bundle originalExtras = extras.getBundle("pushBundle");
 
-			PushPlugin.sendExtras(originalExtras);
-		}
-	}
+      originalExtras.putBoolean("foreground", false);
+      originalExtras.putBoolean("coldstart", !isPushPluginActive);
 
-	/**
-	 * Forces the main activity to re-launch if it's unloaded.
-	 */
-	private void forceMainActivityReload()
-	{
-		PackageManager pm = getPackageManager();
-		Intent launchIntent = pm.getLaunchIntentForPackage(getApplicationContext().getPackageName());    		
-		startActivity(launchIntent);
-	}
+      PushPlugin.sendExtras(originalExtras);
+    }
+  }
+
+  /**
+   * Forces the main activity to re-launch if it's unloaded.
+   */
+  private void forceMainActivityReload() {
+    PackageManager pm = getPackageManager();
+    Intent launchIntent = pm.getLaunchIntentForPackage(getApplicationContext().getPackageName());
+    startActivity(launchIntent);
+  }
 
   @Override
   protected void onResume() {
